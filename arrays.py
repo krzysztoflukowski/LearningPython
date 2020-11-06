b0a9dff7dfb424dedba43438362f87111849e278
Krzysztof Łukowski <krzysztof.lukowski@gmail.com>
2020-11-06 16:01:34 +0100

    arrays tests

---
+++ arrays.py
@@ -0,0 +1,15 @@
+from array import array
+
+moja_tablica = array("i", [])
+
+moja_tablica.append(1)
+moja_tablica.append(1)
+moja_tablica.append(1)
+moja_tablica.append(1)
+moja_tablica.append(1)
+
+print(moja_tablica)
+
+
+for i in moja_tablica:
+    print(i)
